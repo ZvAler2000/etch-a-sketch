ef6302ce3838f729fad8dbd7337380905596bbdb
ZvAler2000 <aleric.zvonimir@gmail.com>
2025-02-12 04:12:41 +0100

    Adjusted the grid so that its dimensions are fixed. Added a button that shows a prompt to change a number of cells per row.

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 let grid = document.getElementById("grid");
+let changeGrid = document.getElementById("changeGrid");
+let gridN = 16;
 
-for(let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row")
-
-    for(let j = 0; j < 16; j++) {
-        const pixel = document.createElement("div");
-        pixel.classList.add("pixel");
-        
-        pixel.addEventListener("mouseover", (event) => {
-            pixel.style.background = "black";
-        });
-
-        row.appendChild(pixel);
+function createGrid() {
+    grid.innerHTML = '';
+    for(let i = 0; i < gridN; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row")
+    
+        for(let j = 0; j < gridN; j++) {
+            const pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+            
+            pixel.addEventListener("mouseover", (event) => {
+                pixel.style.background = "black";
+            });
+    
+            row.appendChild(pixel);
+        }
+    
+        grid.appendChild(row);
     }
-
-    grid.appendChild(row);
 }
 
+changeGrid.addEventListener("click", () => {
+    gridN = prompt("Please input dimensions of grid: ");
+
+    while(gridN <= 0 || gridN > 100) {
+        gridN = prompt("Please input dimensions of grid: ");
+    }
+
+    createGrid();
+})
+
 console.log(grid.children);
+createGrid();
